e4be2dd42d48cd2b8e34bea044969ecbac70ebd9
pkaodev <polsson.development@gmail.com>
2024-07-06 18:50:41 +0100

    123123

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 curl https://github.com/pkaodev/Manageur/raw/main/manageur_setup.zip -o manageur_setup.zip \
-&& unzip manageur_setup.zip \
+&& unzip manageur_setup.zip -d setup_manageur \
 && rm manageur_setup.zip \
-&& setup_manageur/run.sh
+&& setup_manageur/src/run.sh
